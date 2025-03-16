c693638ce2a42d55660d6e846f11f56f02c8e1b9
Kishore <kishore.agilecoach@gmail.com>
2025-03-16 17:42:45 -0400

    Commit of my apple program

---
+++ src/com/kishore/java/apples.java
@@ -0,0 +1,14 @@
+package com.kishore.java;
+
+public class apples {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 10;
+				double j = 10.55;
+				 j = j + i;
+				System.out.println(j);
+
+	}
+
+}
