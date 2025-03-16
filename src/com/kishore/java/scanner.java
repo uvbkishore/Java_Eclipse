4171d7923f828d30caeeb515cca4c7b49e2f5dbe
Kishore <kishore.agilecoach@gmail.com>
2025-03-16 19:39:06 -0400

    Scanner Commit

---
+++ src/com/kishore/java/scanner.java
@@ -0,0 +1,12 @@
+package com.kishore.java;
+import java.util.Scanner;
+
+public class scanner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner bucky = new Scanner(System.in);
+		System.out.println(bucky.nextLine());
+	}
+
+}
